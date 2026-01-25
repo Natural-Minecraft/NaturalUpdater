e0d4c6fb26dddee5cf96ee7595afe50193159ae4
Natural-Minecraft <naturalsmp.mc@gmail.com>
2026-01-25 12:17:41 +0700

    fix: remove stray brace causing syntax error in DownloadUtils

--- src/main/java/id/naturalsmp/naturalUpdater/DownloadUtils.java
+++ src/main/java/id/naturalsmp/naturalUpdater/DownloadUtils.java
@@ -35,16 +35,19 @@ public class DownloadUtils {
                             while ((read = is.read(buffer)) != -1) {
                                 fos.write(buffer, 0, read);
                             }
-                            }
-                            org.bukkit.Bukkit.getLogger().info("[NaturalUpdater] Download complete: " + targetFile.getAbsolutePath());
+                            org.bukkit.Bukkit.getLogger()
+                                    .info("[NaturalUpdater] Download complete: " + targetFile.getAbsolutePath());
                             return targetFile;
                         } catch (Exception e) {
-                            org.bukkit.Bukkit.getLogger().severe("[NaturalUpdater] Failed to write file: " + e.getMessage());
+                            org.bukkit.Bukkit.getLogger()
+                                    .severe("[NaturalUpdater] Failed to write file: " + e.getMessage());
                             e.printStackTrace();
                         }
                     } else {
-                        org.bukkit.Bukkit.getLogger().warning("[NaturalUpdater] HTTP error downloading: " + response.statusCode());
+                        org.bukkit.Bukkit.getLogger()
+                                .warning("[NaturalUpdater] HTTP error downloading: " + response.statusCode());
                     }
                     return null;
                 });
-}}
+    }
+}
