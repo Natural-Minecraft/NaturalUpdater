6a8ff52f05e4c7f489ea8b1452001cbfe53af3a4
Natural-Minecraft <naturalsmp.mc@gmail.com>
2026-01-25 11:37:53 +0700

    chore: replace System.out with Bukkit Logger to fix nag warnings

--- src/main/java/id/naturalsmp/naturalUpdater/DownloadUtils.java
+++ src/main/java/id/naturalsmp/naturalUpdater/DownloadUtils.java
@@ -35,16 +35,16 @@ public class DownloadUtils {
                             while ((read = is.read(buffer)) != -1) {
                                 fos.write(buffer, 0, read);
                             }
-                            System.out.println("[NaturalUpdater] Download complete: " + targetFile.getAbsolutePath());
+                            }
+                            org.bukkit.Bukkit.getLogger().info("[NaturalUpdater] Download complete: " + targetFile.getAbsolutePath());
                             return targetFile;
                         } catch (Exception e) {
-                            System.err.println("[NaturalUpdater] Failed to write file: " + e.getMessage());
+                            org.bukkit.Bukkit.getLogger().severe("[NaturalUpdater] Failed to write file: " + e.getMessage());
                             e.printStackTrace();
                         }
                     } else {
-                        System.err.println("[NaturalUpdater] HTTP error downloading: " + response.statusCode());
+                        org.bukkit.Bukkit.getLogger().warning("[NaturalUpdater] HTTP error downloading: " + response.statusCode());
                     }
                     return null;
                 });
-    }
-}
+}}
